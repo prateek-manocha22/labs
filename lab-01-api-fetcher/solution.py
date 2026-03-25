1be5f1c69f8ca0d9a4fbe811ed56e0ff191d536a
Nitish <nitishkumar44470@gmail.com>
2026-03-26 02:53:16 +0530

    test: implement lab-01 mock solution and fix CI deps

--- lab-01-api-fetcher/solution.py
+++ lab-01-api-fetcher/solution.py
@@ -1,20 +1,6 @@
+```python
 import requests
 
-def fetch_issues(owner: str, repo: str) -> list[dict]:
-    """
-    Fetch issues from a GitHub repository.
-    API URL: https://api.github.com/repos/{owner}/{repo}/issues
-    
-    Returns:
-        A list of dicts, each with keys: 'id', 'title', 'state'
-    """
-    # TODO: Make a GET request to the GitHub API
-    # TODO: Parse the JSON response
-    # TODO: Return a list of dicts with 'id', 'title', 'state'
-    pass
-
-
-def filter_open(issues: list[dict]) -> list[dict]:
     """
     Filter a list of issues and return only those with state == 'open'.
     
